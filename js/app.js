386fef3147458243955e4691142716da78813620
abdallahalomar1 <>
2021-04-14 21:42:46 +0000

    .

---
+++ js/app.js
@@ -0,0 +1,16 @@
+alert("welcome to my website");
+var username=prompt("please enter your favarate car small car or large car");
+if(username=="smallcar"){
+
+ var image1='<img src="img/2.jpg"alt="smallcar"/>'
+document.write(image1);
+
+}
+if(username=="largecar"){
+
+var image2='<img src="https://images.honestjohn.co.uk/imagecache/file/width/640/media/11844111/volvo-xc90-1.jpg"alt="largecar"/>';
+document.write(image2);
+
+
+}
+

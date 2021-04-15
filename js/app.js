019f333259ea633b7e39feb0da130d6fc9c7736c
abdallahalomar1 <>
2021-04-15 12:43:03 +0000

    add

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
 alert("welcome to my website");
-var username=prompt("please enter your favarate car small car or large car");
-if(username=="smallcar"){
+var username =prompt("please enter your favarate car small car or large car");
+while( username!=='small'&& username!=='large')
+{ 
+   username=prompt("please enter your favarate car small car or large car");
+}
+if(username=="small"){
 
- var image1='<img src="img/2.jpg"alt="smallcar"/>'
+ var image1='<img src="img/2.jpg"alt="smallcar"/>';
 document.write(image1);
 
 }
-if(username=="largecar"){
+if(username=="large"){
 
 var image2='<img src="https://images.honestjohn.co.uk/imagecache/file/width/640/media/11844111/volvo-xc90-1.jpg"alt="largecar"/>';
 document.write(image2);
 
+} 
+var number= prompt("how many car favarate");
 
+for(var i=0;i<number;i++)
+{
+  var image2='<img src="https://images.honestjohn.co.uk/imagecache/file/width/640/media/11844111/volvo-xc90-1.jpg"alt="largecar"/>';
+   document.write(image2);
 }
 
+ 

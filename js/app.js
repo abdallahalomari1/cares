2513d1a1fe91017ac3329e290f5c9bcb4dcba589
abdallahalomar1 <>
2021-04-18 18:33:57 +0000

    add

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 alert("welcome to my website");
-var username =prompt("please enter your favarate car small car or large car");
-while( username!=='small'&& username!=='large')
+var typecar =prompt("please enter your favarate car small car or large car");
+console.log(typecar);
+while( typecar!=='small'&& typecar!=='large')
 { 
-   username=prompt("please enter your favarate car small car or large car");
+   typecar=prompt("please enter your favarate car small car or large car");
 }
-if(username=="small"){
+if(typecar=="small"){
 
  var image1='<img src="img/2.jpg"alt="smallcar"/>';
 document.write(image1);
 
 }
-if(username=="large"){
+if(typecar=="large"){
 
 var image2='<img src="https://images.honestjohn.co.uk/imagecache/file/width/640/media/11844111/volvo-xc90-1.jpg"alt="largecar"/>';
 document.write(image2);

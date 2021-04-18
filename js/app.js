da54bf2e825c7084960600ddecef6d3791d7cc56
abdallahalomar1 <>
2021-04-18 13:50:11 +0000

    add file

--- js/app.js
+++ js/app.js
@@ -23,5 +23,17 @@ for(var i=0;i<number;i++)
   var image2='<img src="https://images.honestjohn.co.uk/imagecache/file/width/640/media/11844111/volvo-xc90-1.jpg"alt="largecar"/>';
    document.write(image2);
 }
+//function decleration
+function favcar (car){
+var userchoice=prompt('do you like '+car+'?');
+}
+favcar('limzon');
+// function expression
+var favcar2= function(car){
+var userchoice =prompt('do you like'+car+'?');
+return userchoice ;
+}
+var rusult= favcar2('Toyota');
+console.log(rusult);
 
  
